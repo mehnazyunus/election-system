d245d7c0f6714e92771438c7a53474f0f72df64a
mehnazyunus <mehnaz138@gmail.com>
2018-03-12 15:20:54 +0530

    urls modified

--- election/urls.py
+++ election/urls.py
@@ -2,11 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('election/', include('election.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
     path('accounts/signup/candidate/', views.CandidateSignUpView.as_view(), name='candidate_signup'),
     path('accounts/signup/voter/', views.VoterSignUpView.as_view(), name='voter_signup'),
-
 ]
--- electionsystem/urls.py
+++ electionsystem/urls.py
@@ -20,9 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('election/', include('election.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
-    # path('accounts/signup/candidate/', views.candidateSignUpView.as_view(), name='candidate_signup'),
-    # path('accounts/signup/voter/', views.voterSignUpView.as_view(), name='voter_signup'),
-
 ]
